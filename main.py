26acb4e3671e6f436b474cd007627ca521f9a4e0
Alex Yang <zy5f9@mail.missouri.edu>
2019-08-14 18:02:00 -0500

    wrap up

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from pathlib import Path
 import numpy as np
 import tensorflow as tf
 import cv2
+from global_vars import *
 
 def test_hires(ctx):
     tf.keras.backend.clear_session()
-
     with tf.variable_scope('DataSouce'):
         def mapper(fname_im):
             im = tf.image.decode
